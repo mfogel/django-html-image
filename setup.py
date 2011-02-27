3b5e94f7dd0270d98fdd54b45980bd77e93790c6
Mike Fogel <mike@fogel.ca>
2011-02-26 19:54:47 -0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='django-html-image',
+    version='0.1',
+    description='Django image models and helpers for common use cases.',
+    author='Mike Fogel',
+    author_email='mike@fogel.ca',
+    url='http://github.com/carbonXT/django-html-image',
+    packages=['html_image'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Utilities',
+    ],
+) 
