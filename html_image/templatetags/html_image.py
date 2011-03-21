530f83390af6327751cd932afbc695de7949fd07
Mike Fogel <mike@fogel.ca>
2011-03-21 16:17:43 -0700

    graceful failure on missing img file

--- html_image/templatetags/html_image.py
+++ html_image/templatetags/html_image.py
@@ -8,12 +8,10 @@ register = template.Library()
 
 @register.filter
 def html_image_tag(html_image):
-    if not isinstance(html_image, BaseHtmlImage):
-        m = _("Given '{g}' of type {tg}, expected {te}").format(
-                g=html_image, tg=type(html_image).__name__,
-                te=BaseHtmlImage.__name__)
-        raise TypeError(m)
     # valid html req's both alt and src tags.
-    html = u'<img alt="{0}" src="{1}" />'.format(
-            html_image.alt_display, html_image.image.url)
+    try:
+        html = u'<img alt="{0}" src="{1}" />'.format(
+                html_image.alt_display, html_image.image.url)
+    except:
+        html = u''
     return mark_safe(html)
